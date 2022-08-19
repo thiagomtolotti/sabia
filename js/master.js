c6a34a67630f8b00f68832d040ae2acdcd51a1cc
thiago <thiagotolotti@gmail.com>
2022-08-19 18:48:16 -0300

    Outras páginas criadas, inciando JS. parei criando a função 'renderNav()'

---
+++ js/master.js
@@ -0,0 +1,25 @@
+function showMenu(){
+  document.querySelector("#navbar").style.left = 0
+}
+
+function closeMenu(){
+    document.querySelector("#navbar").style.left = "-100vw"
+}
+
+function renderNav(){
+  // let nav = document.createElement("nav");
+  // nav.id = "navbar";
+  //
+  // let mobileNav = document.createElement("nav");
+  // mobileNav.id = "mobile_nav";
+  //
+  // document.body.insertAdjacentHTML("afterbegin", nav)
+}
+
+renderNav()
+
+/*
+TODO
+- renderNav
+- JSON Populate
+*/
